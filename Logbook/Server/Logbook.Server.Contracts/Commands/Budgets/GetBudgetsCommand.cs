45d09d7dbe033fa587af1d53ac4e8328aae7ac80
Daniel Häfele <haefele@xemio.net>
2016-01-10 13:16:18 +0100

    Add command to retrieve a users budgets

---
+++ Logbook/Server/Logbook.Server.Contracts/Commands/Budgets/GetBudgetsCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LiteGuard;
+using Logbook.Shared.Entities.Budgets;
+
+namespace Logbook.Server.Contracts.Commands.Budgets
+{
+    public class GetBudgetsCommand : ICommand<IList<Budget>>
+    {
+        public string ForUserId { get; }
+
+        public GetBudgetsCommand(string forUserId)
+        {
+            Guard.AgainstNullArgument(nameof(forUserId), forUserId);
+
+            this.ForUserId = forUserId;
+        }
+    }
+}
---
+++ Logbook/Server/Logbook.Server.Infrastructure/Raven/Indexes/Budgets_ByUserId.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Logbook.Shared.Entities.Budgets;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace Logbook.Server.Infrastructure.Raven.Indexes
+{
+    public class Budgets_ByUserId : AbstractIndexCreationTask<Budget>
+    {
+        public Budgets_ByUserId()
+        {
+            this.Map = budgets => 
+                from budget in budgets
+                select new
+                {
+                    budget.ForUserId
+                };
+
+            this.Index(f => f.ForUserId, FieldIndexing.NotAnalyzed);
+        }
+    }
+}
