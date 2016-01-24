03ab4cab7715ccd64f97d7c4963aef5c3e44900c
Daniel Häfele <haefele@xemio.net>
2016-01-24 20:53:21 +0100

    Improve summoners list layout and add ability to delete a summoner

--- frontend/src/summoner-overview.ts
+++ frontend/src/summoner-overview.ts
@@ -4,6 +4,7 @@ import {autoinject} from "aurelia-framework";
 import {SummonersService} from "services/summoners-service";
 import {Summoner} from "api/models/summoner";
 import {IAddSummonerData} from "components/add-summoner";
+import {Region} from "api/models/region";
 
 @autoinject()
 export class SummonerOverview {
@@ -18,6 +19,11 @@ export class SummonerOverview {
     }
     
     public async addSummoner(data: IAddSummonerData) : Promise<void> {
-         this.summoners = await this.summonersService.addSummoner(data.region, data.summonerName);
+        this.summoners = await this.summonersService.addSummoner(data.region, data.summonerName);
+    }
+    
+    public async deleteSummoner(summoner: Summoner): Promise<void> {
+        let region = Region.get(summoner.region);        
+        this.summoners = await this.summonersService.deleteSummoner(region, summoner.id);
     }
 }
