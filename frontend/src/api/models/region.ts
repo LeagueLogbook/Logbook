9735268e6794815046ca83eb7f95c93cdf8e9e15
Daniel Häfele <haefele@xemio.net>
2016-01-24 20:04:30 +0100

    Some general improvements with regions in the client

--- frontend/src/api/models/region.ts
+++ frontend/src/api/models/region.ts
@@ -11,48 +11,79 @@ export class Region {
         return _.head(this.getAll().filter(f => f.shortName.toLowerCase() === shortName.toLowerCase()));
     }
     
+    public static get BR(): Region {
+        return {
+            shortName: "BR",
+            name: "Brazil",
+        };
+    } 
+    public static get EUNE(): Region {
+        return {
+            shortName: "EUNE",
+            name: "Europe Nordic & East",    
+        };
+    }
+    public static get EUW(): Region {
+        return {
+            shortName: "EUW",    
+            name: "Europe West",
+        };
+    }
+    public static get NA(): Region {
+        return {
+            shortName: "NA",  
+            name: "North America",  
+        };
+    }
+    public static get KR(): Region {
+        return {
+            shortName: "KR",
+            name: "Korea",    
+        };
+    }
+    public static get LAN(): Region {
+        return {
+            shortName: "LAN",
+            name: "Latin America North",    
+        };
+    }
+    public static get LAS(): Region {
+        return {
+            shortName: "LAS",
+            name: "Latin America South",    
+        };
+    }
+    public static get OCE(): Region {
+        return {
+            shortName: "OCE",
+            name: "Oceania",    
+        };
+    }
+    public static get RU(): Region {
+        return {
+            shortName: "RU",
+            name: "Russia",    
+        };
+    }
+    public static get TR(): Region {
+        return {
+            shortName: "TR",
+            name: "Turkey",    
+        };
+    }
+    
     public static getAll() : Region[] {
         return [
-            {
-                shortName: "BR",
-                name: "Brazil",
-            },
-            {
-                shortName: "EUNE",
-                name: "Europe Nordic & East",    
-            },
-            {
-                shortName: "EUW",    
-                name: "Europe West",
-            },
-            {
-                shortName: "NA",  
-                name: "North America",  
-            },
-            {
-                shortName: "KR",
-                name: "Korea",    
-            },
-            {
-                shortName: "LAN",
-                name: "Latin America North",    
-            },
-            {
-                shortName: "LAS",
-                name: "Latin America South",    
-            },
-            {
-                shortName: "OCE",
-                name: "Oceania",    
-            },
-            {
-                shortName: "RU",
-                name: "Russia",    
-            },
-            {
-                shortName: "TR",
-                name: "Turkey",    
-            },
+            this.BR, 
+            this.EUNE,
+            this.EUW,
+            this.NA,
+            this.KR,
+            this.LAN,
+            this.LAS,
+            this.OCE,
+            this.RU,
+            this.TR,
         ];
     }
 }
