eb57e4985c57dc572d50031ad4da553fe2a9d469
Daniel Häfele <haefele@xemio.net>
2016-01-27 22:14:08 +0100

    Add command and button-to-command custom attribute
    This is for issue #21.

---
+++ frontend/src/helper/command.ts
@@ -0,0 +1,38 @@
+"use strict";
+
+export class Command {
+    
+    private _isExecuting: boolean;
+    
+    public get isExecuting(): boolean {
+        return this._isExecuting;
+    }
+    
+    public get canExecute(): boolean {
+        let can = this.onCanExecute();
+        return can;
+    }
+    
+    public constructor(private onExecute: () => Promise<void>, private onCanExecute: () => boolean) {        
+    }
+    
+    public async execute(): Promise<void> {
+        
+        if (this.isExecuting === true) {
+            return;
+        }
+        
+        if (this.canExecute === false) {
+            return;            
+        }
+        
+        try {
+            this._isExecuting = true;
+            
+            await this.onExecute();
+        }
+        finally {
+            this._isExecuting = false;
+        }
+    }
+}
