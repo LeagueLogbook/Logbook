3c6270d3089ba9d54a58487ad451b74019a870b2
Daniel Häfele <haefele@xemio.net>
2016-02-07 15:30:23 +0100

    Add equals and gethashcode implementation in the aggregate-root

--- backend/Shared/Logbook.Shared/Entities/AggregateRoot.cs
+++ backend/Shared/Logbook.Shared/Entities/AggregateRoot.cs
@@ -1,7 +1,29 @@
-﻿namespace Logbook.Shared.Entities
+﻿using System;
+
+namespace Logbook.Shared.Entities
 {
-    public abstract class AggregateRoot
+    public abstract class AggregateRoot : IEquatable<AggregateRoot>
     {
         public virtual int Id { get; set; }
+
+        public bool Equals(AggregateRoot other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return this.Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return this.Equals((AggregateRoot)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id;
+        }
     }
 }
