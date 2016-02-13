b94372c5893040638aa46482a366ad8694d47c07
Daniel Häfele <haefele@xemio.net>
2016-02-13 19:02:39 +0100

    Implement the azure worker

--- backend/Azure/Logbook.Azure.Worker/WorkerRole.cs
+++ backend/Azure/Logbook.Azure.Worker/WorkerRole.cs
@@ -1,35 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using Logbook.Worker.Api;
 using Microsoft.WindowsAzure.ServiceRuntime;
 
 namespace Logbook.Azure.Worker
 {
     public class WorkerRole : RoleEntryPoint
     {
+        private CancellationTokenSource _cancellationTokenSource;
+        private ApiWorker _apiWoker;
+
         public override bool OnStart()
         {
             // Set the maximum number of concurrent connections
-            ServicePointManager.DefaultConnectionLimit = 256;
+            ServicePointManager.DefaultConnectionLimit = 100;
 
+            var container = new WindsorContainer();
+            container.Install(FromAssembly.InThisApplication());
+
+            this._cancellationTokenSource = new CancellationTokenSource();
+
+            this._apiWoker = container.Resolve<ApiWorker>();
+
+            Task.WaitAll(
+                this._apiWoker.StartAsync());
 
             return true;
         }
 
         public override void Run()
         {
-     
+            Task.WaitAll(
+                this._apiWoker.RunAsync(this._cancellationTokenSource.Token));
         }
 
         public override void OnStop()
         {
-            
+            this._cancellationTokenSource.Cancel();
         }
     }
 }
